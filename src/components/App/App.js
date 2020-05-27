d5b2d9b20ff7952b33a244e0628b2ff00800087b
Paulina Kuranowska <kuranowskapaulina@gmail.com>
2020-05-27 16:17:58 +0200

    modul 11.5 - stan komponentu - start Creator

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './App.scss';
+import List from '../List/List.js';
+import {pageContents, listData} from '../../data/dataStore';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main className={styles.component}>
+        <h1 className={styles.title}>{pageContents.title}</h1>
+        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+        {/*<List title={['Things to do', <sup key='1'>soon!</sup>]} image={'https://i.postimg.cc/QxYj0QpB/space.png'}></List>*/}
+        <List {...listData} />
+      </main>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/List/List.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styles from './List.scss';
+import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column.js';
+import { settings } from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
+
+class List extends React.Component {
+    state = {
+    columns: this.props.columns || [],
+    }
+    
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        description: PropTypes.node,
+        columns: PropTypes.array,
+      }
+
+    static defaultProps = {
+        description: settings.defaultListDescription,
+      }
+
+  render() {
+    return (
+      <section className={styles.component}>
+        <Hero titleText={this.props.title} HeroImage={this.props.image} />
+        <div className={styles.description}>
+        {ReactHtmlParser(this.props.description)}
+        </div>
+        <div className={styles.columns}>
+          {this.state.columns.map(({key, ...columnProps}) => (
+          <Column key={key} {...columnProps} />
+          ))}
+        </div>
+      </section>
+    )
+  }
+}
+
+export default List;
