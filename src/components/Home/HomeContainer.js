6c61477c3936656b88a8ce5346bf955aa986ba3b
Paulina Kuranowska <kuranowskapaulina@gmail.com>
2020-06-09 17:23:28 +0200

    modul 13.2 - Dodajemy nawigacje

--- src/components/Home/HomeContainer.js
+++ src/components/Home/HomeContainer.js
@@ -1,5 +1,5 @@
 import {connect} from 'react-redux';
-import App from './App';
+import Home from './Home.js';
 
 const mapStateToProps = state => ({
   title: state.app.title,
@@ -7,6 +7,6 @@ const mapStateToProps = state => ({
   lists: state.lists,
 }); {/* w stałej mapStateToProps zapisujemy funkcję, która definiuje powiązanie propsów z stanem */}
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(Home);
 {/* wyrażenie jest odpowiedzialne za połączenie komponentu App z magazynem,
 w ostatniej parze nawiasów musimy podać komponent, który jest wykorzystywany w tym kontenerze. */}
--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,14 @@ const baseConfig = () => ({
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
+    publicPath: '/',
     filename: 'scripts_bundle.js',
   },
+
+  devServer: {
+    historyApiFallback: true,
+  },
+  
   module: {
     rules: [
       {
